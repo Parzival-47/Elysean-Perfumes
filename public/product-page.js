535fa9a8921bae0349a74952d35c080695c9754d
Parzival-47 <bcarel001@gmail.com>
2026-07-27 10:45:03 +0200

    Test Payment

--- public/product-page.js
+++ public/product-page.js
@@ -67,7 +67,7 @@ const PRODUCTS = [
             {
                 "ml": "Sample",
                 "label": "",
-                "price": 50
+                "price": 5
             },
             {
                 "ml": "30ml",
